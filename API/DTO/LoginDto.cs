a5cf6908be8034f069fb7e094c3b0e1a75c16ee1
nguyentdat27 <nguyentdat27@gmail.com>
2022-01-15 20:38:33 +0700

    test multiple account

--- API/DTO/LoginDto.cs
+++ API/DTO/LoginDto.cs
@@ -8,7 +8,7 @@ namespace API.DTO
     public class LoginDto
     {
         public string Email { get; set; }
-    public string UserName { get; set; }
+        public string UserName { get; set; }
         public string Password { get; set; }
     }
 }
